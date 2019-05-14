43a74976895eab576e52d1b4de34adbfb6b290c5
Jerry <chih.chia.chuang@gmail.com>
2019-05-14 21:40:45 +0000

    character counter function complete

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
 $(document).ready(function() {
   // --- our code goes here ---
+  $('.new-tweet textarea[name = "text"]').on('input', function () {
+    let count = 140 - $(this).val().length;
+    $(this).siblings('.counter').text(count);
+    if (count < 0){
+      $(this).siblings('.counter').css("color", "red")
+    } else {
+      $(this).siblings('.counter').css("color", "#244751")
+    }
+  })
 });

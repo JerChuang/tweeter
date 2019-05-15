1dd0be0fe73a0cad35c923e2d4b5d527fb6a4c7d
Jerry <chih.chia.chuang@gmail.com>
2019-05-15 04:00:09 +0000

    finished create tweet element function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,6 +24,39 @@ const tweetData = {
   "created_at": 1461116232227
 }
 
+function createTweetElement(Object){
+  // Object.user.name = name in header
+  // Object.user.avatars.regular = avatar image in header
+  // Object.user.handle = handle in header
+  // Object.content.text = main text
+  // Object.created_at = created time
+  let $output = $('<article></article>');
+  let $header = $('<header></header>');
+  let $image = $(`<img class="avatar" src=${Object.user.avatars.regular} /> `)
+  let $handle = $('<span></span').addClass('handle').text(Object.user.handle);
+  let $content = $('<div></div>').addClass('content').text(Object.content.text);
+  let $footer = $('<footer></footer>').text(Object.created_at).append(`<img class="like" src="/images/heart.png" />`);
+
+  $output = $output.append($header.append($image).append(Object.user.name, $handle)).append($content).append($footer);
+
+return $output
+}
+
+/* <article>
+      <header>
+        <img class="avatar" src="/images/bird.png" /> Bill Reid
+        <span class="handle"> @ReidB </span>
+      </header>
+      <div class = "content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+          labore et dolore magna aliquaasdfhb d d f f 140
+      </div>         
+      <footer>
+        10 days ago <img class="like" src="/images/heart.png" /> 
+      </footer>
+  </article> */
+
+
 var $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)

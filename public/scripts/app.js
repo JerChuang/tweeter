177adde8042d842abe946bbc12b4beef57aec6b0
Jerry <chih.chia.chuang@gmail.com>
2019-05-15 01:57:37 +0000

    added twitter regular state styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,21 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+$(document).ready(function() {
+  // --- our code goes here ---
+  
+  
+  // $('.new-tweet textarea[name = "text"]').on('input', function () {
+  //   let count = 140 - $(this).val().length;
+  //   $(this).siblings('.counter').text(count);
+  //   if (count < 0){
+  //     $(this).siblings('.counter').css("color", "red")
+  //   } else {
+  //     $(this).siblings('.counter').css("color", "#244751")
+  //   }
+  // })
+});

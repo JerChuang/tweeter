89de1afcce88a1510d332a69b1dd2fecf5feea48
Jerry <chih.chia.chuang@gmail.com>
2019-05-15 23:47:48 +0000

    added error messages for empty tweet or too-long tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,53 +7,7 @@
  */
 $(document).ready(function() {
   // --- our code goes here ---
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1557935797394
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //       },
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Johann von Goethe",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //       },
-  //       "handle": "@johann49"
-  //     },
-  //     "content": {
-  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //     },
-  //     "created_at": 1461113796368
-  //   }
-  // ];
-  
+
   function renderTweets(Data) {
     for (object of Data) {
       $('.tweets').append(createTweetElement(object))
@@ -73,7 +27,7 @@ $(document).ready(function() {
     } else if (time / (1000 * 60) > 1){
       return `${Math.floor(time / (1000 * 60))} minutes ago`
     } else if (time / (1000) > 1){
-      return `${Math.floor(time / (1000))} seconds ago`
+      return `${Math.floor(time / 1000)} seconds ago`
     } else {
       return 'just now'
     }
@@ -92,17 +46,25 @@ $(document).ready(function() {
 
     return $output
   }
-
-  // renderTweets(data);
   
   $('section.new-tweet form').on('submit', function(event){
     event.preventDefault();
     let content = $(this).serialize();
+    if (!content.slice(5)){
+      window.alert('Please enter a message!')
+      return;
+    }
+    let count = Number($('section.new-tweet span.counter').text());
+    if ( count < 0) {
+      window.alert('tweets must be under 140 char')
+      return;
+    }
     
     $.post('/tweets', content);
+    this.reset();
     loadTweets()
   });
-
+  
   function loadTweets(){
     $.ajax({
       url: '/tweets',
@@ -114,27 +76,9 @@ $(document).ready(function() {
       }
     });
   }
-  loadTweets();
-  
-  
-//   <form method = "POST" action = "/tweets">
-//   <textarea name="text" placeholder="What are you humming about?"></textarea>
-//   <input type="submit" value="Tweet">
-//   <span class="counter">140</span>
-// </form>
-  
 
-  // $(function() {
-  //   var $button = $('#load-more-posts');
-  //   $button.on('click', function () {
-  //     console.log('Button clicked, performing ajax call...');
-  //     $.ajax('more-posts.html', { method: 'GET' })
-  //     .then(function (morePostsHtml) {
-  //       console.log('Success: ', morePostsHtml);
-  //       $button.replaceWith(morePostsHtml);
-  //     });
-  //   });
-  // });
+  loadTweets();
+ 
 
 
 

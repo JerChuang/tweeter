e506c0e5e4eabc413efe72d4e1bb8a7dca554d99
Jerry <chih.chia.chuang@gmail.com>
2019-05-16 03:23:28 +0000

    new tweet area now automatically focus of text area when displaying

--- public/scripts/compose-button.js
+++ public/scripts/compose-button.js
@@ -3,10 +3,15 @@
 
 $(document).ready(function() {
   // --- our code goes here ---
+
+  let displayingForm = true; //checking if we're hiding the form or showing the form
   $('#nav-bar .compose').on('click', function () {
-    $( ".new-tweet" ).slideToggle( "slow", function() {
-      // Animation complete.
-    });
+    $( ".new-tweet" ).slideToggle( "slow");
+    displayingForm = !displayingForm;
+    if(displayingForm){   //if we're showing the form, we want to focus on the textarea
+      $(".new-tweet textarea").focus();  
+    }
+    
   })
 });
 
